f04715a4f9e90554f3ddfbc9f79e0839cc4e62e4
Yoarly Castillo M <yoita5@MacBook-Pro-de-Yoarly.local>
2023-10-28 19:59:03 -0400

    grafos

---
+++ EDDproyecto1/src/grafos/Assets.java
@@ -0,0 +1,23 @@
+
+package grafos;
+
+public class Assets {
+    public String getFileNameExtension(String fullName) {
+        int dotIndex = fullName.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : fullName.substring(dotIndex + 1);
+    }
+    
+    public static boolean isInteger(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int result = Integer.parseInt(strNum);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
+
+    
